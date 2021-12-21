61ab5d36af7433045e46c9f26eb50053b05a5f6b
rakhmatillo16 <rahmatillorasulov2000@gmail.com>
2021-12-21 11:00:16 +0500

    prompt corrected

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const userName = prompt("Salom, ismingiz nima?")
-const X =  Number (alert ("ozingizda qancha pul borligini kiriting! (sumda)"));
+const X =  Number (prompt ("ozingizda qancha pul borligini kiriting! (sumda)"));
 const sumDollar = 750*10896.61;
 const sumEuro = 120*12247.79;
 const costs = Math.round (Number (sumDollar+sumEuro));
